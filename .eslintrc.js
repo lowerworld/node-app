3ee7fa42ba9472d57915c4348b3ab8f440328386
Kei Yoshimura <11.sigsegv@gmail.com>
2022-07-25 21:01:16 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
